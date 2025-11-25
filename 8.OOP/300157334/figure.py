dadab03dc56a641e37ac6dcb310826210449cfba
laalahamdaoui16-alt <laalahamdaoui16@gmail.com>
2025-11-25 15:16:32 -0500

    -
    Updated date format in the file header and added a comment for clarity.

--- 8.OOP/300157334/figure.py
+++ 8.OOP/300157334/figure.py
@@ -1,14 +1,13 @@
-
 """
 Fichier : figure.py
 Description : Classe de base pour toutes les figures géométriques
 Auteur : [300157334]
-Date : 19-11-2025
+Date : 2025-11-19
 """
 
 class Figure:
     def __init__(self, nom):
-      
+        # Nom de la figure (ex: Carré, Cercle)
         self.nom = nom
 
     def afficher_info(self):
@@ -18,4 +17,3 @@ class Figure:
     def aire(self):
         # Méthode à implémenter par les sous-classes
         raise NotImplementedError("Cette méthode doit être implémentée par les sous-classes.")
-
