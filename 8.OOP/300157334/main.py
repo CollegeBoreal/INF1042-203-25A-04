8f8d9b68c6f0acb5939b2a708b052a677018c714
laalahamdaoui16-alt <laalahamdaoui16@gmail.com>
2025-11-25 15:48:05 -0500

    -
    Updated import statements and improved comments for clarity.

--- 8.OOP/300157334/main.py
+++ 8.OOP/300157334/main.py
@@ -1,11 +1,12 @@
 """
 Fichier : main.py
-Description : Point d'entrée du programme. Crée un carré, un cercle et un cylindre, puis affiche leurs informations.
+Description : Point d'entrée du programme. 
+Crée un carré, un cercle et un cylindre puis affiche leurs informations.
 Auteur : [300157334]
 Date : 2025-11-19
 """
 
-from Carre import Carre
+from carre import Carre
 from Cercle import Cercle
 from Cylindre import Cylindre
 
@@ -14,21 +15,21 @@ def main():
     Fonction principale du programme.
     Crée un carré, un cercle et un cylindre, puis affiche leurs informations.
     """
+
     # Création d'un carré de côté 4
     c1 = Carre(4)
 
     # Création d'un cercle de rayon 3
     c2 = Cercle(3)
 
-    # Création d'un cylindre de rayon 2 et hauteur 5
+    # Création d'un cylindre : rayon = 2, hauteur = 5
     c3 = Cylindre(2, 5)
 
-    # Affichage des informations
+    # Affichage des informations des trois figures
     print(c1.afficher_info())
-    print()
     print(c2.afficher_info())
-    print()
     print(c3.afficher_info())
+
 
 # Point d'entrée du programme
 if __name__ == "__main__":
