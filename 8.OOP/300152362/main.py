0656b41c8138a69c68e15a59547b76095ef60e46
naser674 <tekomeg374@gmail.com>
2025-11-26 18:54:01 -0500

    Update main.py

--- 8.OOP/300152362/main.py
+++ 8.OOP/300152362/main.py
@@ -5,8 +5,8 @@ Auteur : [300152362]
 Date : 2025-11-26
 """
 
-from Carre import Carre
-from Cercle import Cercle
+from carre import Carre
+from cercle import Cercle
 from Triangle import Triangle 
 
 def main():
