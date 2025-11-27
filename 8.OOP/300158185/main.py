b64f8ccc9bf1fe06dc6c584d4efb588d384989ec
Mohammed-mati <mohmati1998@gmail.com>
2025-11-26 20:15:09 -0500

    Include Triangle class and update date
    Added Triangle class to main program and updated date.

--- 8.OOP/300158185/main.py
+++ 8.OOP/300158185/main.py
@@ -2,11 +2,12 @@
 Fichier : main.py
 Description : Point d'entrée du programme. Crée un carré et un cercle et affiche leurs informations.
 Auteur : [300158185]
-Date : 2025-11-19
+Date :2025-11-26
 """
 
 from Carre import Carre
 from Cercle import Cercle
+from Triangle import Triangle 
 
 def main():
     """
@@ -25,4 +26,7 @@ def main():
 
 # Point d'entrée du programme
 if __name__ == "__main__":
+    formes = [Carre(4), Cercle(3), Triangle(5, 2)]
+for f in formes:
+    print(f"Aire: {f.aire()} 📏")
     main()
