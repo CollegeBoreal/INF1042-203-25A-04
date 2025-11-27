64875af9aeed30e8978eeea552973fad5c1fa52d
hama374 <djaoudhamama15@gmail.com>
2025-11-26 19:34:34 -0500

    Update date in main.py

--- 8.OOP/300153811/main.py
+++ 8.OOP/300153811/main.py
@@ -2,7 +2,7 @@
 Fichier : main.py
 Description : Point d'entrée du programme. Crée un carré et un cercle et affiche leurs informations.
 Auteur : [300153811]
-Date : 2025-11-24
+Date : 2025-11-19
 """
  
 from Carre import Carre
