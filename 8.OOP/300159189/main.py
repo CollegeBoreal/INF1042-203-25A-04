91e277a9ac21150dcf3b6159ec342d9b30f3e4fe
Fateh98 <fatehboutaghane98@gmail.com>
2025-12-03 14:03:48 -0800

    Update main.py

--- 8.OOP/300159189/main.py
+++ 8.OOP/300159189/main.py
@@ -1,33 +1,19 @@
-"""
-Fichier : main.py
-Description : Point d'entrée du programme. Crée un carré et un cercle et affiche leurs informations.
-Auteur : [300159189]
-Date : 2025-12-03
-"""
- 
-from Carre import Carre
-from Cercle import Cercle
+from carre import Carre
+from cercle import Cercle
 from Triangle import Triangle
+from trapeze import Trapeze
 
 def main():
-    """
-    Fonction principale du programme.
-    Crée un carré et un cercle, puis affiche leurs informations.
-    """
-    # Création d'un carré de côté 4
-    c1 = Carre(4)
+    formes = [
+        Carre(4),
+        Cercle(3),
+        Triangle(5, 2),
+        Trapeze(6, 4, 3)
+    ]
 
-    # Création d'un cercle de rayon 3
-    c2 = Cercle(3)
+    for f in formes:
+        print(f.afficher_info())
+        f.dessiner()
 
-    # Affichage des informations des deux figures
-    print(c1.afficher_info())
-    print(c2.afficher_info())
-
-# Point d'entrée du programme
 if __name__ == "__main__":
-    formes = [Carre(4), Cercle(3), Triangle(5, 2)]
-for f in formes:
-    print(f"Aire: {f.aire()} 📏")
     main()
-
