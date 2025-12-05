c1a4a94520af1fa8fad1d0d064390cb1ebbbedca
ERNICE 778 <erniceken3@gmail.com>
2025-12-04 19:39:22 -0500

    ajout de fichier main.py

---
+++ 8.OOP/300155281/main.py
@@ -0,0 +1,23 @@
+"""
+Fichier : main.py
+Description : Point d'entrée du programme. Crée un carré et un cercle et affiche leurs informations.
+Auteur : [300155281]
+Date : 2025-12-04
+"""
+
+from Carre import Carre
+from Cercle import Cercle
+from Trapeze import Trapeze
+
+def main():
+    formes = [
+        Carre(4),
+        Cercle(10),
+        Trapeze(6, 4, 8)
+    ]
+
+    for f in formes:
+        print(f.afficher_info())
+
+if _name_ == "_main_":
+    main()
