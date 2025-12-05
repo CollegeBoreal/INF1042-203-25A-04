6bd2dd1d9c8b8cb5131cb9cda5fd94804a5078dd
Mohammed-mati <mohmati1998@gmail.com>
2025-12-05 17:15:17 -0500

    Rename class Cercle to cercle

--- 8.OOP/300158185/cercle.py
+++ 8.OOP/300158185/cercle.py
@@ -1,5 +1,5 @@
 """
-Fichier : Cercle.py
+Fichier : cercle.py
 Description : Classe Cercle héritant de Figure
 Auteur : [300158185]
 Date : 1998-08-10
@@ -8,9 +8,9 @@ Date : 1998-08-10
 from figure import Figure
 import math
 
-class Cercle(Figure):
+class cercle(Figure):
     def __init__(self, rayon):
-        super().__init__("Cercle")  # Appel du constructeur de la classe de base
+        super().__init__("cercle")  # Appel du constructeur de la classe de base
         self.rayon = rayon           # Rayon du cercle
 
     def aire(self):
