57cdb65f8c569a836f1994aa4ce803ea7967f4f6
Alexia <alexiakabore2@gmail.com>
2025-12-08 18:08:03 -0500

    Ajout notebook figures géométriques + graphismes

--- 8.OOP/300155109/main.py
+++ 8.OOP/300155109/main.py
@@ -1,28 +1,30 @@
 """
 Fichier : main.py
-Description : Point d'entrée du programme. Crée un carré et un cercle et affiche leurs informations.
+Description : Point d'entrée du programme. Crée un carré, un cercle et un trapeze et affiche leurs informations.
 Auteur : 300155109
 Date : 2025-12-07
 """
 
 from carre import Carre
 from cercle import Cercle
+from trapeze import Trapeze
+from hexagone import Hexagone
 
 def main():
-    """
-    Fonction principale du programme.
-    Crée un carré et un cercle, puis affiche leurs informations.
-    """
-    # Création d'un carré de côté 4
-    c1 = Carre(4)
+    # Création des figures
+    f1 = Carre(4)
+    f2 = Cercle(3)
+    f3 = Trapeze(5, 7, 4)  # base1=5, base2=7, hauteur=4
+    f4 = Hexagone(4)
 
-    # Création d'un cercle de rayon 3
-    c2 = Cercle(3)
 
-    # Affichage des informations des deux figures
-    print(c1.afficher_info())
-    print(c2.afficher_info())
+    # Liste polymorphique
+    formes = [f1, f2, f3, f4]
 
-# Point d'entrée du programme
+    # Affichage
+    for f in formes:
+        print(f.afficher_info())
+
+# 🔥 Indispensable pour exécuter main()
 if __name__ == "__main__":
     main()
