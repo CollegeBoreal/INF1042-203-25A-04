f612c3faf7446b5185ea7bf94a78a8a9c3ee54fe
BADReddine223 <badrbarragoub539@gmail.com>
2025-12-11 00:21:39 -0500

    Update Triangle.py

--- 8.OOP/300155504/Triangle.py
+++ 8.OOP/300155504/Triangle.py
@@ -11,6 +11,7 @@ __student_id__ = "300155504"
 
 from figure import Figure
 import matplotlib.pyplot as plt
+from math import sqrt
 
 
 class Triangle(Figure):
@@ -22,33 +23,21 @@ class Triangle(Figure):
         self.hauteur = hauteur
 
     def aire(self) -> float:
-        """Retourne l'aire du triangle."""
         return (self.base * self.hauteur) / 2
 
     def perimetre(self) -> float:
-        """
-        Pour simplifier : triangle rectangle isocèle
-        base = hauteur
-        périmètre = base + hauteur + hypoténuse
-        """
-        from math import sqrt
-
+        """On suppose un triangle rectangle isocèle."""
         hyp = sqrt(self.base**2 + self.hauteur**2)
         return self.base + self.hauteur + hyp
 
     def dessiner(self, axe: plt.Axes) -> None:
-        """
-        Dessine un triangle rectangle :
-        (0,0), (base,0), (0,hauteur)
-        """
+        """Dessine un triangle rectangle : (0,0), (base,0), (0,hauteur)."""
         xs = [0, self.base, 0, 0]
         ys = [0, 0, self.hauteur, 0]
         axe.plot(xs, ys)
         axe.set_aspect("equal")
-
         marge_x = self.base * 0.2
         marge_y = self.hauteur * 0.2
-
         axe.set_xlim(-marge_x, self.base + marge_x)
         axe.set_ylim(-marge_y, self.hauteur + marge_y)
         axe.set_title(f"Triangle (base={self.base}, hauteur={self.hauteur})")
