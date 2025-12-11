84ca87b60ba469c43df417f62c3867f8da0f1581
BADReddine223 <badrbarragoub539@gmail.com>
2025-12-11 05:13:48 +0100

    Update Cercle.py

--- 8.OOP/300155504/Cercle.py
+++ 8.OOP/300155504/Cercle.py
@@ -1,10 +1,11 @@
+# Signature : BadrEddine Barragoub - 300155504
+
 """
 Fichier : Cercle.py
 Description : Classe Cercle héritant de Figure.
-Auteur : BadrEddine Barragoub (300155504)
+Auteur : 300155504
 Date : 2025-12-10
 """
-
 
 from figure import Figure
 import math
@@ -15,8 +16,7 @@ class Cercle(Figure):
         self.rayon = rayon
 
     def aire(self):
-        # Aire = π * r²
-        return math.pi * self.rayon ** 2
+        return math.pi * (self.rayon ** 2)
 
     def afficher_info(self):
         return f"{super().afficher_info()}, rayon={self.rayon}, aire={self.aire():.2f}"
