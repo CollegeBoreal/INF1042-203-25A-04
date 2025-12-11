02960a06a275c2e59608bd8a18b8108660210fb3
Ayoteck <ameurazzeddine07@gmail.com>
2025-12-10 21:28:52 -0600

    Ajout de la classe Triangle et mise à jour de main.py

--- 8.OOP/300147253/main.py
+++ 8.OOP/300147253/main.py
@@ -1,30 +1,16 @@
-"""
-Fichier : main.py
-Description : Point d'entrée du programme. Crée un carré et un cercle et affiche leurs informations.
-Auteur : 300147253
-Date : 2025-12-09
-"""
-
 from Carre import Carre
 from Cercle import Cercle
+from Triangle import Triangle  # <- ajouter cette ligne
 
 def main():
-    """
-    Fonction principale du programme.
-    Crée un carré et un cercle, puis affiche leurs informations.
-    """
-    # Création d'un carré de côté 4
     c1 = Carre(4)
-
-    # Création d'un cercle de rayon 3
     c2 = Cercle(3)
+    t1 = Triangle(5, 2)  # <- créer un triangle
 
-    # Affichage des informations des deux figures
     print(c1.afficher_info())
     print(c2.afficher_info())
+    print(t1.afficher_info())  # <- afficher les infos du triangle
 
-# Point d'entrée du programme
 if __name__ == "__main__":
     main()
- == "__main__":
-    main()
+
